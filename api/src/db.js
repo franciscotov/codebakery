4a4a81c4af20cc87d1c7603eca7a85f57a950eac
franciscotov <frank.ronaldo_17@hotmail.com>
2023-09-07 15:12:10 -0300

    cambio de base de datos v2

--- api/src/db.js
+++ api/src/db.js
@@ -12,7 +12,6 @@ if(process.env.DATABASE_URL){
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
     dialect: "mysql",
-    protocol: "mysql",
     dialectOptions: {
       native: true,
       ssl: false
